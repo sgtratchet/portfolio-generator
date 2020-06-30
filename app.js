7dab6d2c0be0d2a26b4efd8729c0ba1e4eadb30c
Devin Ellsworth <devin.ellsworth@power-innovations.com>
2020-06-30 15:31:15 -0600

    created page template

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-  console.log(profileDataArr[i]);
-  }
-  console.log('================');
+// console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  
-  printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
