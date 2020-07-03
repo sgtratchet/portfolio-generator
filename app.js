0d4d6c97ca767dfb746e0c512f882009d6f37cf7
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-07-02 21:56:39 -0600

    added ability for multiple projects

--- app.js
+++ app.js
@@ -75,9 +75,18 @@ const promptProject = portfolioData => {
         message: 'Would you like to enter another project?',
         default: false
       }
-    ]);
+    ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
   };
-promptUser()
-.then(answers => console.log(answers))
-.then(promptProject)
-.then(projectAnswers => console.log(projectAnswers));
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
