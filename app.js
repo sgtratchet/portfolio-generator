d602ab8d87a2dd8b7c704808a17705bb065de949
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-07-05 23:39:14 -0600

    added css file

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ const promptProject = portfolioData => {
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err => {
+    fs.writeFile('./dist/index.html', pageHTML, err => {
       if (err) throw new Error(err);
 
       console.log('Page created! Check out index.html in this directory to see it!');
